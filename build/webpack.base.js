d6f71f2b44f08a4a63a70a863cbfaf8416f35c29
winoooops <winoooops@outlook.com>
2021-09-03 11:24:31 +0800

    webpack config

---
+++ build/webpack.base.js
@@ -0,0 +1,46 @@
+const webpack = require('webpack')
+const path = require('path')
+const sass = require('sass')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader')
+
+const resolve = (filePath) => path.resolve(__dirname, filePath)
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        filename: 'bundle',
+        path: resolve('dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.(png|jpeg|jpg|gif)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        limit: 10000,
+                        name: 'assets/[name].[hash:8].[ext]',
+                    }
+                }],
+            },
+            {
+                test: /\/.sass$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ]
+            }
+        ]
+    }
+}
